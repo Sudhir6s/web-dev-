c570cdd9589d90cc1b6f88fdb3a48475726d07bc
Sudhir6s <157670293+Sudhir6s@users.noreply.github.com>
2024-12-15 06:58:56 +0000

    ok

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12343
+let accountEmail = "sudhir@google.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 2 //not allowed
+accountEmail = "sh@sh.com"
+accountPassword = "213454"
+accountCity = "bengaluru"
+let accountState;
+console.log(accountId);
+/*
+
+*/
+
+console.table ([accountId,accountEmail,accountPassword,accountCity,accountState])
