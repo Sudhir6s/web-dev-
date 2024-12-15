db2f4214da31d2b8b392dcdaffec5a749e4d8fee
Sudhir6s <157670293+Sudhir6s@users.noreply.github.com>
2024-12-15 13:07:42 +0000

    ok

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3+3) // we are using node jd not browser
+ 
+console.log(3+3) //code readability shoudbe high
+
+console.log("sudhir")
+
+let name = "sudhir"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 
+// bigint
+// string=>""
+// boolean=> teue/false
+// undefined=> unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log (typeof null); //object
