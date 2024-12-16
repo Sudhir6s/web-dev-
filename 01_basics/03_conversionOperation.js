267e124778ca9b250161d9aaf9429cf908b69f78
Sudhir6s <157670293+Sudhir6s@users.noreply.github.com>
2024-12-16 05:11:03 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score = "33abc" //NaN
+//let score = null // 0
+//let score = undefined //NaN
+//let score = true//1
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+//1= true; 0 = false 
+//"" = false 
+//"guddu" = true
+
+let someNumber = 47
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
